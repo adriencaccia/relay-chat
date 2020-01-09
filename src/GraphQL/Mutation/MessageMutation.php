be731ceac689ac82abd20be426e15cf12ede0a8a
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2020-01-09 17:00:11 +0100

    ✨ Add message mutation

---
+++ src/GraphQL/Mutation/MessageMutation.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\GraphQL\Mutation;
+
+use App\Entity\Message;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+
+class MessageMutation implements MutationInterface, AliasedInterface
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    /** @var UserRepository */
+    private $userRepository;
+
+    public function __construct(EntityManagerInterface $em, UserRepository $userRepository)
+    {
+        $this->em = $em;
+        $this->userRepository = $userRepository;
+    }
+
+    public function postMessage(string $text, int $userId): array
+    {
+        $user = $this->userRepository->find($userId);
+
+        $message = new Message();
+        $message
+            ->setCreatedAt(new \DateTime())
+            ->setText($text)
+            ->setUser($user)
+        ;
+
+        $this->em->persist($message);
+        $this->em->flush();
+
+        return [
+            'message' => $message,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getAliases(): array
+    {
+        return [
+            'postMessage' => 'post_message',
+        ];
+    }
+}
