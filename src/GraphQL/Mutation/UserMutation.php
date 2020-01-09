234c2f9ac3addd1100586b73a473b6ce2ce829db
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2020-01-09 09:30:17 +0100

    ✨ Add user mutation

---
+++ src/GraphQL/Mutation/UserMutation.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\GraphQL\Mutation;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\MutationInterface;
+
+class UserMutation implements MutationInterface, AliasedInterface
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function createUser(string $name): array
+    {
+        $user = new User();
+        $user->setName($name);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return [
+            'user' => $user,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getAliases(): array
+    {
+        return [
+            'createUser' => 'create_user',
+        ];
+    }
+}
