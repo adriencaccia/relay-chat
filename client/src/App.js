4e92c46498a0521aaa2a20a2eb17b540d7139703
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2020-01-12 17:45:20 +0100

    ✨ Setup relay

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import graphql from "babel-plugin-relay/macro";
+import React from "react";
+import { QueryRenderer } from "react-relay";
+import relayEnvironment from "./relayEnvironment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={relayEnvironment}
+        query={graphql`
+          query AppQuery {
+            users {
+              edges {
+                node {
+                  name
+                }
+                cursor
+              }
+            }
+          }
+        `}
+        variables={{}}
+        render={({ error, props }) => {
+          if (error) {
+            return <div>Error!</div>;
+          }
+          if (!props) {
+            return <div>Loading...</div>;
+          }
+          return (
+            <div>
+              <div>Number of users: {props.users.edges.length}</div>
+              <ul>
+                {props.users.edges.map(edge => (
+                  <li key={edge.cursor}>{edge.node.name}</li>
+                ))}
+              </ul>
+            </div>
+          );
+        }}
+      />
+    );
+  }
 }
-
-export default App;
