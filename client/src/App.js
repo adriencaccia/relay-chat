99e89eba3ef845791e8d6be8a193d24b09e806fd
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2020-02-20 20:45:02 +0100

    ✨ Add SimpleUser demo

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,22 @@ import graphql from "babel-plugin-relay/macro";
 import React from "react";
 import { QueryRenderer } from "react-relay";
 import SimpleUser from "./components/SimpleUser";
-import SlowComponent from "./components/SlowComponent";
 import relayEnvironment from "./relayEnvironment";
+
+const appQuery = graphql`
+  query AppQuery {
+    node(id: "5e31876721e45") {
+      ...SimpleUser_user
+    }
+  }
+`;
 
 export default class App extends React.Component {
   render() {
     return (
       <QueryRenderer
         environment={relayEnvironment}
-        query={graphql`
-          query AppQuery {
-            node(id: "5e31876721e45") {
-              ...SimpleUser_user
-            }
-            ...SlowComponent_slowResult @arguments(timeout: 1500)
-          }
-        `}
+        query={appQuery}
         variables={{}}
         render={({ error, props }) => {
           if (error) {
@@ -26,12 +26,7 @@ export default class App extends React.Component {
           if (!props) {
             return <div>Loading...</div>;
           }
-          return (
-            <div>
-              <SimpleUser user={props.node} />
-              <SlowComponent slowResult={props} />
-            </div>
-          );
+          return <SimpleUser user={props.node} />;
         }}
       />
     );
